41923b739e952254cec552918e178b072260dd98
Ezra Crowe <ecrowe@oberlin.edu>
2024-05-09 15:08:44 -0400

    sorta working now

--- react-midi-display/src/Midi-Display/Midi-Display.js
+++ react-midi-display/src/Midi-Display/Midi-Display.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Midi from 'midi-player-js';
+import './Midi-Display.css';
 
 
 function isIterable(obj) {
@@ -34,85 +35,128 @@ export { buildNotes };
 function Midi_Display({ midiFilePath }) {
     const [numNotes, setNumNotes] = useState([0,0]);
     const [notes, setNotes] = useState([]);
+    const [totalTicks, setTotalTicks] = useState(0);
+    const [keyWidth, setKeyWidth] = useState(25);
+    const [keyHeight, setKeyHeight] = useState(100);
 
-    useEffect(() => {
-        const fetchMidiFile = async () => {
-            console.log(`Fetching MIDI file from ${midiFilePath}`);
-            const response = await fetch(midiFilePath);
-            const arrayBuffer = await response.arrayBuffer();
-            const player = new Midi.Player();
-            player.loadArrayBuffer(arrayBuffer);
-            console.log('events ${}', player.getEvents());
-            let lowest = Infinity;
-            let highest = -Infinity;
-            let noteToNoteState = {};
-            //Consists of three fields where the first is the noteName the second is its duration in ticks and the third is its notenumber
-            let notes = [];
-            const events = player.getEvents();
-            events.forEach(event => {
-                if (isIterable(event))
-                {
-                    event.forEach(e => {
-                        //Note off is not a needed check as the library only displays things as note on :'(
-                        //Keeping the check around though in case they fix the library
-                        if (e.name == 'Note on' || e.name == 'Note off') {
-                            if (noteToNoteState[e.noteNumber] == undefined) {
-                                noteToNoteState[e.noteNumber] = [e.noteName, true, e.tick]
+    const fetchMidiFile = async () => {
+        console.log(`Fetching MIDI file from ${midiFilePath}`);
+        const response = await fetch(midiFilePath);
+        const arrayBuffer = await response.arrayBuffer();
+        const player = new Midi.Player();
+        player.loadArrayBuffer(arrayBuffer);
+        console.log('events ${}', player.getEvents());
+        let lowest = Infinity;
+        let highest = -Infinity;
+        let noteToNoteState = {};
+        //Consists of three fields where the first is the noteName the second is its duration in ticks and the third is its notenumber
+        let notes = [];
+        let totalTicks = 0;
+        const events = player.getEvents();
+        events.forEach(event => {
+            if (isIterable(event))
+            {
+                event.forEach(e => {
+                    //Note off is not a needed check as the library only displays things as note on :'(
+                    //Keeping the check around though in case they fix the library
+                    if (e.name == 'Note on' || e.name == 'Note off') {
+                        if (e.tick > totalTicks) {
+                            setTotalTicks(e.tick);
+                        }
+                        if (noteToNoteState[e.noteNumber] == undefined) {
+                            noteToNoteState[e.noteNumber] = [e.noteName, true, e.tick]
+                        }else
+                        {
+                            if (noteToNoteState[e.noteNumber][1] == true)
+                            {
+                                noteToNoteState[e.noteNumber][1] = false;
+                                notes.push([e.noteName, e.tick - noteToNoteState[e.noteNumber][2], e.noteNumber]);
                             }else
                             {
-                                if (noteToNoteState[e.noteNumber][1] == true)
-                                {
-                                    noteToNoteState[e.noteNumber][1] = false;
-                                    notes.push([e.noteName, e.tick - noteToNoteState[e.noteNumber][2], e.noteNumber]);
-                                }else
-                                {
-                                    noteToNoteState[e.noteNumber][1] = true;
-                                    noteToNoteState[e.noteNumber][2] = e.tick;
-                                }
-                            }
-                            if (e.noteNumber < lowest) {
-                                lowest = e.noteNumber;
-                            }
-                            if (e.noteNumber > highest) {
-                                highest = e.noteNumber;
+                                noteToNoteState[e.noteNumber][1] = true;
+                                noteToNoteState[e.noteNumber][2] = e.tick;
                             }
                         }
-                    });
-                }
-            });
-            console.log(notes);
-            console.log(`Lowest note: ${lowest}, Highest note: ${highest}`);
-            console.log(`Loaded ${highest - lowest} numNotes from MIDI file`);
-            setNumNotes([lowest, highest]);
-            setNotes(notes);
-        };
-    
+                        if (e.noteNumber < lowest) {
+                            lowest = e.noteNumber;
+                        }
+                        if (e.noteNumber > highest) {
+                            highest = e.noteNumber;
+                        }
+                    }
+                });
+            }
+        });
+        console.log(notes);
+        console.log(`Lowest note: ${lowest}, Highest note: ${highest}`);
+        console.log(`Loaded ${highest - lowest} numNotes from MIDI file`);
+        console.log(`Total ticks: ${totalTicks}`);
+
+        let blackKeyNum = 0;
+
+        for (let i = numNotes[0]; i <= numNotes[1]; i++)
+        {
+            //1 3 6 8 10 are all the black keys
+            if (i % 12 == 1 || i % 12 == 3 || i % 12 == 6 || i % 12 == 8 || i % 12 == 10)
+            {
+                blackKeyNum++;//
+            }
+        }
+        console.log("window size: " + window.innerWidth);
+        console.log("white notes: " + (numNotes[1] - numNotes[0] - blackKeyNum));
+        console.log("notelength " + parseInt(window.innerWidth / (numNotes[1] - numNotes[0] - blackKeyNum + 1)) * (numNotes[1] - numNotes[0] - blackKeyNum + 1));
+
+        setKeyWidth(window.innerWidth / (numNotes[1] - numNotes[0] - blackKeyNum + 1))// - 1 - (1 / (numNotes[1] - numNotes[0] - blackKeyNum)));
+        setNumNotes([lowest, highest]);
+        setNotes(notes);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', fetchMidiFile)
         fetchMidiFile();
+        return () => {
+            window.removeEventListener('resize', fetchMidiFile)
+        }
     }, [midiFilePath]);
 
     
 
     return (
-        <div data-testid="midi-display-content" style={{ overflowY: 'scroll', overflowX: 'scroll', whiteSpace: 'nowrap', display: 'flex', flexDirection: 'row', alignItems: 'flex-start' }}>
-            
+        <div style={{ overflowY: 'scroll', overflowX: 'scroll', whiteSpace: 'nowrap', display: 'flex', flexDirection: 'row', alignItems: 'flex-start'}}>
+            { (() => {
+                console.log("num notes: " + numNotes[0] + " " + numNotes[1]);
+                console.log("noteWidth: " + keyWidth);
+                
+                fetchMidiFile();
+                let divArray = [];
+                let size = 1;
+                divArray.push(<div key={-1} style={{position: 'absolute', left: 0, display: 'inline-block', width: '100%', height: (size * 1 * totalTicks) + 'px', backgroundColor: 'grey', zIndex: -1}}></div>);
+                return divArray;
+            } )()}
+
             { (() => {
                 let divArray = [];
-                let width = 25;
-                let height = 100;
                 let whiteNum = 0;
                 let lastWhiteKeyPosition = 0;
+
                 for (let i = numNotes[0]; i <= numNotes[1]; i++)
                 {
                     //1 3 6 8 10 are all the black keys
                     if (i % 12 == 1 || i % 12 == 3 || i % 12 == 6 || i % 12 == 8 || i % 12 == 10)
                     {
-                        divArray.push(<div key={i} style={{position: 'absolute', left: (lastWhiteKeyPosition + (width + 1) - (width / 4))+ 'px', display: 'inline-block', width: (width / 2)+'px', height: (height * (5/8))+'px', backgroundColor: 'black', zIndex: 2}}></div>);
+                        divArray.push(<div key={i} style={{position: 'absolute', left: (lastWhiteKeyPosition + (keyWidth + 1) - (keyWidth / 4))+ 'px', display: 'inline-block', width: (keyWidth / 2)+'px', height: (keyHeight * (5/8))+'px', backgroundColor: 'black', zIndex: 2}}></div>);
                     }else{
-                        lastWhiteKeyPosition = (width + 1) * whiteNum;
+                        lastWhiteKeyPosition = (keyWidth) * whiteNum;
+                        //console.log("lastWhiteKeyPosition after: " + lastWhiteKeyPosition);
+                        console.log("key width" + keyWidth);
+                        //console.log("total space: " + ((keyWidth + 1) * whiteNum));
                         whiteNum++;
-                        divArray.push(<div key={i} style={{position: 'absolute', left: lastWhiteKeyPosition, border: "1px solid black", display: 'inline-block', width: width+'px', height: height+'px', backgroundColor: 'white'}}></div>);
+                        console.log("lastWhiteKeyPosition: " + lastWhiteKeyPosition);
+                        divArray.push(<div key={i} style={{position: 'absolute', left: lastWhiteKeyPosition + 'px', boxShadow: "inset 0 0 0 1px black", display: 'inline-block', width: keyWidth+'px', height: keyHeight+'px', backgroundColor: 'white'}}></div>);
                     }
                 }
+                console.log(whiteNum);
+                console.log("window size: " + window.innerWidth);
                 return divArray;
                 //<div key={index} style={{ border: "5px solid black", display: 'inline-block', width: '20px', height: '20px', backgroundColor: 'blue'}}></div>
             })()}
